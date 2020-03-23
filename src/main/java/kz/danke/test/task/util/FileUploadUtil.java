05596008c10c9bba37764e78485a03fd75a1fe70
Vladislav Dankevich <vielendanke1991@gmail.com>
2020-03-23 11:24:36 +0600

    file upload util

---
+++ src/main/java/kz/danke/test/task/util/FileUploadUtil.java
@@ -0,0 +1,30 @@
+package kz.danke.test.task.util;
+
+import kz.danke.test.task.exceptions.WrongDateException;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+public class FileUploadUtil {
+
+    public static String fileUpload(MultipartFile imageFile, String filePath) throws IOException {
+        String resultFileName;
+
+        if (imageFile == null) {
+            throw new WrongDateException("File cannot be empty");
+        } else {
+            File uploadDir = new File(filePath);
+
+            if (!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            resultFileName = uuidFile + "." + imageFile.getOriginalFilename();
+
+            imageFile.transferTo(new File(filePath + "/" + resultFileName));
+        }
+        return resultFileName;
+    }
+}
