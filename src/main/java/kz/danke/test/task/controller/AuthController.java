2a82cbe03d3258a8298775af72db4901b68c35cd
Vladislav Dankevich <vielendanke1991@gmail.com>
2020-04-01 11:06:13 +0600

    controller binding results for validation error

--- src/main/java/kz/danke/test/task/controller/AuthController.java
+++ src/main/java/kz/danke/test/task/controller/AuthController.java
@@ -8,19 +8,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
@@ -39,6 +35,7 @@ public class AuthController {
         if (bindingResult.hasErrors()) {
             Map<String, String> errors = ControllerUtil.getErrors(bindingResult);
             model.mergeAttributes(errors);
+            return "registration";
         }
 
         user.setImageName(fileUploadUtil.fileUpload(user.getMultipartFile(), filePath));
@@ -51,8 +48,11 @@ public class AuthController {
     @GetMapping("/activate/{code}")
     public String activateAccount(HttpServletRequest request, @PathVariable String code) {
         User user = userService.findByActivationCode(code);
+
         request.getSession().setAttribute("activate", "Successfully activated");
+
         userService.save(user);
+
         return "redirect:/login";
     }
 }
--- src/main/java/kz/danke/test/task/controller/MessageController.java
+++ src/main/java/kz/danke/test/task/controller/MessageController.java
@@ -2,15 +2,20 @@ package kz.danke.test.task.controller;
 
 import kz.danke.test.task.model.Message;
 import kz.danke.test.task.security.CustomUserDetails;
+import kz.danke.test.task.security.annotations.IsApprovedPerson;
+import kz.danke.test.task.security.annotations.IsAuthorized;
 import kz.danke.test.task.service.MessageService;
 import kz.danke.test.task.service.UserService;
+import kz.danke.test.task.util.ControllerUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.security.Principal;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -20,10 +25,18 @@ public class MessageController {
     private final UserService userService;
 
     @PostMapping("add_message")
+    @IsAuthorized
     public String addMessage(
             @RequestParam(name = "messageText") String messageText,
-            @AuthenticationPrincipal CustomUserDetails authUser
+            @AuthenticationPrincipal CustomUserDetails authUser,
+            BindingResult bindingResult,
+            Model model
     ) {
+        if (bindingResult.hasErrors()) {
+            Map<String, String> errors = ControllerUtil.getErrors(bindingResult);
+            model.mergeAttributes(errors);
+            return "cabinet";
+        }
         Message message = new Message();
 
         message.setDescription(messageText);
